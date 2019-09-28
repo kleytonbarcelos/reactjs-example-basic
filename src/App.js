85324e389d6d6e2b50c43c4c31f3bdd48b4177f9
Kleyton Barcelos <kleytonbarcelos@gmail.com>
2019-09-28 17:49:27 -0400

    Module 04 - Learn ReactJS of zero

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import TechList from "./components/TechList";
+import "./App.css";
+
+import profile from "./assets/profile.jpg";
+
+function App() {
+  return <TechList />;
+}
+
+export default App;
